49978b2d1f9925c3f04ff2837ce9771b3064759f
Jason Sears <jsears4585@gmail.com>
2017-06-20 12:06:29 -0400

    Login functionality, routing, session creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :activity_entries
+  validates :username, uniqueness: true
+  # validates :email, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/signup', to: 'users#new', as: 'signup'
+  resources :users, only: [:create, :show, :edit, :update, :destroy]
+
   resources :activity_entries
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
