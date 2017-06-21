961e689dc94d13a2f53edc7b531201f682cff010
Jason Sears <jsears4585@gmail.com>
2017-06-21 13:05:26 -0400

    Made emailer dynamic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = 'http://localhost:3000/login'
-    mail(to: "jsears4585@gmail.com", subject: 'Welcome to HealthBattles.com!')
+    mail(to: @user.email, subject: 'Welcome to HealthBattles.com!')
   end
 end
