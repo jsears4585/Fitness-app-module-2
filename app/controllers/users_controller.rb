67f6348b5703b49687567d730cc6ebacdc1cc740
Jason Sears <jsears4585@gmail.com>
2017-06-21 15:44:22 -0400

    Added dashboard controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,59 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login, except: [:new, :create, :new_admin]
 
-  def index
-    @users = User.all
-    @chart = Fusioncharts::Chart.new({
-      width: "600",
-      height: "400",
-      type: "pie3d",
-      renderAt: "chartContainer",
-      dataSource: {
-        "chart": {
-          "caption": "Split of User Sign-ups",
-          "subCaption": "Yesterday vs. Today",
-          "paletteColors": "#0075c2,#1aaf5d,#f2c500,#f45b00,#8e0000",
-          "bgColor": "#ffffff",
-          "showBorder": "0",
-          "use3DLighting": "0",
-          "showShadow": "0",
-          "enableSmartLabels": "0",
-          "startingAngle": "0",
-          "showPercentValues": "1",
-          "showPercentInTooltip": "0",
-          "decimals": "1",
-          "captionFontSize": "14",
-          "subcaptionFontSize": "14",
-          "subcaptionFontBold": "0",
-          "toolTipColor": "#ffffff",
-          "toolTipBorderThickness": "0",
-          "toolTipBgColor": "#000000",
-          "toolTipBgAlpha": "80",
-          "toolTipBorderRadius": "2",
-          "toolTipPadding": "5",
-          "showHoverEffect": "1",
-          "showLegend": "1",
-          "legendBgColor": "#ffffff",
-          "legendBorderAlpha": "0",
-          "legendShadow": "0",
-          "legendItemFontSize": "10",
-          "legendItemFontColor": "#666666",
-          "useDataPlotColorForLabels": "1"
-        },
-        "data": [
-          {
-              "label": "Yesterday",
-              "value": User.where("created_at < ?", Time.zone.now.beginning_of_day).count
-          },
-          {
-              "label": "Today",
-              "value": User.where("created_at >= ?", Time.zone.now.beginning_of_day).count
-          }
-        ]
-      }
-    })
-  end
-
   def new
     @user = User.new
   end
@@ -155,9 +102,6 @@ class UsersController < ApplicationController
   end
 
   # Admin actions
-
-  def admin
-  end
 
   def new_admin
     @user = User.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'dashboard/new'
+
   resources :challenges
   root 'welcome#index'
 
@@ -8,12 +10,12 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   get '/signup', to: 'users#new', as: 'signup'
-  resources :users, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:create, :show, :edit, :update, :destroy]
 
   get '/entries/new', to: 'activity_entries#new', as: 'new_entry'
   resources :activity_entries, only: [:create, :edit]
-  
-  get '/users/:id/admin', to: 'users#admin', as: "admin_user"
+
+  get '/dashboard', to: 'dashboard#new', as: "dashboard"
 
   get 'signup/admin', to: 'users#new_admin', as: 'admin_signup'
   post 'signup/admin', to: 'users#create'
